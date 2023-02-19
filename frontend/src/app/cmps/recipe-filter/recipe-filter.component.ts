f8a517abd33a1d8cd1f6eb8e67388febfeae68fb
Alon <a.hoffman.lon@gmail.com>
2023-02-19 16:42:32 +0200

    'Checboxes'

--- frontend/src/app/cmps/recipe-filter/recipe-filter.component.ts
+++ frontend/src/app/cmps/recipe-filter/recipe-filter.component.ts
@@ -1,12 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
   selector: 'recipe-filter',
   templateUrl: './recipe-filter.component.html',
   styleUrls: ['./recipe-filter.component.scss']
 })
-export class RecipeFilterComponent implements OnInit {
-  @Input() recipes: any[] = [];
+export class RecipeFilterComponent implements OnInit, OnChanges {
+  @Input() recipes!: Recipe[] 
   @Output() filtered = new EventEmitter<string[]>();
 
   uniqueIngredients: string[] = [];
@@ -15,23 +16,34 @@ export class RecipeFilterComponent implements OnInit {
 
   ngOnInit(): void {
     this.generateIngredientsList();
+    console.log(this.recipes)
   }
+
+  ngOnChanges(changes: any) {
+    if (changes.recipes && changes.recipes.currentValue) {
+      this.generateIngredientsList();
+      console.log(this.recipes);
+    }
+  }
+  
 
   generateIngredientsList(): void {
     const allIngredients = this.recipes.flatMap(recipe => recipe.ingredients);
+    console.log("🚀 ~ file: recipe-filter.component.ts:22 ~ RecipeFilterComponent ~ generateIngredientsList ~ allIngredients", allIngredients)
     const uniqueIngredients = new Set(allIngredients.map(ingredient => ingredient.name));
     this.uniqueIngredients = [...uniqueIngredients];
   }
 
   onFilterChange(): void {
     const checkboxes = document.querySelectorAll('.ingredient-filter');
-    // const checkedBoxes = Array.from(checkboxes)
-    //   .filter((checkbox: HTMLInputElement) => checkbox.checked)
-    //   .map((checkbox: HTMLInputElement) => checkbox.value);
-
+    const checkedBoxes = Array.from(checkboxes)
     //@ts-ignore
-    this.filtered.emit(checkboxes);
-    // this.filtered.emit(checkedBoxes);
+
+      .filter((checkbox: HTMLInputElement) => checkbox.checked)
+    //@ts-ignore
+
+      .map((checkbox: HTMLInputElement) => checkbox.value);
+    this.filtered.emit(checkedBoxes);
   }
 
 }
